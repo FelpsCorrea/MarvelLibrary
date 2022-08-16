54c7fee2c641985efb732917a6fea7c3e870b01e
Felipe Correa Lopes dos Santos <felipe.santos@praisce.com>
2022-08-15 21:14:28 -0300

    nova cor adicionada na palheta depois que o routing funcionou

--- marvel_library/lib/app/themes/opera_theme.dart
+++ marvel_library/lib/app/themes/opera_theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class OperaTheme{
   static const Color darkRed = Color(0xFF700540);
   static const Color mediumRed = Color(0xFFb10b43);
+  static const Color ultraDarkPurple = Color(0xFF160a22);
   static const Color darkPurple = Color(0xFF130544);
   static const Color mediumPurple = Color(0xFF5d4d70);
   static const Color purple = Color(0xFF6548a4);
